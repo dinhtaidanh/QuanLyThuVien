c2f120b171a95677247309ea4138c3d148414ec1
Your Name <1451010024Danh@ou.edu.vn>
2018-03-16 11:44:19 +0700

    tao lop NhanVien

---
+++ NhanVien.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManHinhChinh
+{
+    class NhanVien
+    {
+        public int MaNhanVien { set; get; }
+        public string Ho { set; get; }
+        public string Ten { set; get; }
+        public string ChucVu { set; get; }
+        public string TaiKhoan { set; get; }
+        public string MatKhau { set; get; }
+        
+    }
+}
