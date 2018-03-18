972e927d815cc6f110ce9fa485f86366094cd478
bao <chungbaosc.py@gmail.com>
2018-03-18 06:20:16 -0700

    UnitTestKhachHang

---
+++ UnitTestQLTV/UnitTestKhachHang.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Mail;
+using ManHinhChinh;
+using ManHinhChinh.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestQLTV
+{
+    [TestClass]
+    public class UnitTestKhachHang
+    {
+        KhachHangService khachHangService;
+        [TestInitialize]
+        public void SetUp()
+        {
+        }
+        [TestMethod]
+        public void TestInsertKhachHang()
+        {
+            khachHangService = new KhachHangService();
+            KhachHang khachHang = new KhachHang()
+            {
+                Ho = "123",
+                Ten = "123",
+                DiaChi = "123",
+                Email = "123",
+                SoDienThoai = "123"
+            };
+            var k= khachHangService.InsertKhachHang(khachHang);
+            Assert.IsNotNull(k);
+        }
+    }
+}
