124f3a338591150ea9e66ab594526a7703e5ba65
bao <chungbaosc.py@gmail.com>
2018-03-19 00:45:12 -0700

    form

---
+++ QuanLyDangNhap.cs
@@ -0,0 +1,42 @@
+﻿using ManHinhChinh.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ManHinhChinh
+{
+    public partial class QuanLyDangNhap : Form
+    {
+        NhanVienService nhanVienService;
+        public QuanLyDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnD_Click(object sender, EventArgs e)
+        {
+            nhanVienService = new NhanVienService();
+            try
+            {
+                NhanVien nhanVien = nhanVienService.GetNhanVienByUser(txtTen.Text, txtMatKhau.Text);
+                if(nhanVien!= null)
+                {
+                    ManHinhChinh manHinhChinh = new ManHinhChinh();
+                    manHinhChinh.Show();
+                    Close();
+                }
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng");
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Không có dữ liệu");
+            }
+        }
+    }
+}
